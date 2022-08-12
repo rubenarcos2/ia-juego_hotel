952a1810b70a00fd3bbae998d8e31b3e48f0a389
Rubén Arcos <rubenarcos2@gmail.com>
2022-08-12 11:18:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def main():
                                      result=prediction,
                                      )
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True)
